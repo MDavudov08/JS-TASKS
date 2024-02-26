55196140ab09c6173712e15c86ab93082dac17a3
Murtuz Davudov <murtuzdavudov70@gmail.com>
2024-02-26 14:26:34 +0400

    js-task-11

---
+++ js-task-11/script.js
@@ -0,0 +1,71 @@
+//3?//5?//6?//15?//11
+// 1
+// let number = +prompt("Zəhmət olmasa iki rəqəmli ədəd daxil edin:");
+// if (number >= 10 && number <= 99) {
+//   while (number < 100) {
+//     number += 7;
+//   }
+//   console.log("Son dəyər:", number);
+// } else {
+//   console.log("Yalnış daxiletmə. Zəhmət olmasa iki rəqəmli nömrə daxil edin.");
+// }
+
+//2
+
+// let N = +prompt("Please enter number:");
+// for (let i = 0; i < N; i++) {
+//     console.log("I know how to use cycles");
+// }
+
+//4
+
+// const isValidNumber = (phoneNumber) => phoneNumber.length === 13 && phoneNumber.slice(0, 2) === "+7";
+// console.log(isValidNumber("+791234567892"));
+// console.log(isValidNumber("8123456789"));
+
+//8
+
+// const uCase = (str) => {
+//     let uStr = str.toUpperCase();
+//     console.log(uStr);
+// }
+// uCase("hello world");
+
+//9
+
+// const lCase = (str) => {
+//     let lStr = str.toLowerCase();
+//     console.log(lStr); 
+// }
+// lCase("HELLO WORLD")
+
+//10
+
+// const firstLetterReplace = (word, firstLetter) => {
+//     const newWord = firstLetter + word.slice(1);
+//     return newWord;
+// }
+// console.log(firstLetterReplace('Qara', "Y")); 
+
+//12
+
+// const GmailAddress = (email) => {
+//     return email.endsWith("@gmail.com");
+// }
+// console.log(GmailAddress("abcdf@gmail.com"));
+// console.log(GmailAddress("test@yahoo.com"));
+
+//13
+
+// const AllUppercase = (str) => str === str.toUpperCase();
+// console.log(AllUppercase("HELLO"));
+// console.log(AllUppercase("Hello"));
+
+//14
+
+// const Characters = (sentence) => sentence.slice(0, 5);
+// console.log(Characters("Hello world"));
+
+
+
+
