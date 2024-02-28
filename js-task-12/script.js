6dca71ef2a00c050a3e2e82906eb31ca0e5f4951
Murtuz Davudov <murtuzdavudov70@gmail.com>
2024-02-28 14:18:04 +0400

    js-task-12

---
+++ js-task-12/script.js
@@ -0,0 +1,28 @@
+//1
+// let team = ["Cosqun","Huseyn","Ali"];
+// team.push("Rasul");
+// team.shift();
+// console.log(team);
+
+//2
+
+// let names = ["Neriman","Ayxan","Ferid"]
+// names[3] = "Cavid"
+// console.log(names);
+
+//3
+
+// let arr = ['New Hope', 'The Empire Strikes Back', 'Return of the Jedi'];
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(`Episode ${i + 1}: ${arr[i]}`);
+// }
+
+//4
+// ?
+//5
+
+// let num = '32, 31, 34, 36, 31';
+// let arr = num.split(",")
+// let result = arr.join(";")
+// console.log(result);
+
